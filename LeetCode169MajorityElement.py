f765a06ed56f2a036335bfd38e73b192ddce1fb4
lonely7yk <893077810@qq.com>
2020-05-20 19:10:00 -0700

    LeetCode169 Moore Voting improved

--- LeetCode169MajorityElement.py
+++ LeetCode169MajorityElement.py
@@ -36,13 +36,13 @@ class Solution:
         res = -1
 
         for num in nums:
-            if cnt == 0:
-                res = num
-
-            if num != res:
-                cnt -= 1
-            else:
+            if num == res:
                 cnt += 1
+            elif cnt == 0:
+                res = num
+                cnt = 1
+            else:
+                cnt -= 1
 
         return res
 
