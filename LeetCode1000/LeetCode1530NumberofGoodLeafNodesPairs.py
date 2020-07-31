f2be4eb05a438fa2d39769baad344ca54d7e401f
lonely7yk <893077810@qq.com>
2020-07-30 23:02:32 -0700

    LeetCode1530 DFS improved

--- LeetCode1000/LeetCode1530NumberofGoodLeafNodesPairs.py
+++ LeetCode1000/LeetCode1530NumberofGoodLeafNodesPairs.py
@@ -49,45 +49,71 @@ class TreeNode:
         self.left = left
         self.right = right
 
-# DFS: 对于每一个节点，找他的左节点下所有叶子节点高度和右节点下所有叶子节点高度。对于所有叶子节点的高度，
-# 如果左叶子节点的高度+右叶子节点的高度<=distance，就把 cnt 加 1。（这是叶子节点在左右边的情况）
-# 然后还要加上叶子节点在同一边的情况
+# # DFS: 对于每一个节点，找他的左节点下所有叶子节点高度和右节点下所有叶子节点高度。对于所有叶子节点的高度，
+# # 如果左叶子节点的高度+右叶子节点的高度<=distance，就把 cnt 加 1。（这是叶子节点在左右边的情况）
+# # 然后还要加上叶子节点在同一边的情况
+# # 但这个会多次计算节点高度，时间复杂度高
+# class Solution:
+#     def countPairs(self, root: TreeNode, distance: int) -> int:
+#         def findAllLeafsHeight(node, currHeight, heights):
+#             if not node: return []
+
+#             if not node.left and not node.right:
+#                 heights.append(currHeight)
+#                 return
+
+#             findAllLeafsHeight(node.left, currHeight + 1, heights)
+#             findAllLeafsHeight(node.right, currHeight + 1, heights)
+
+#         def dfs(node, distance):
+#             if not node: return 0
+
+#             cnt = 0
+#             leftHeights = []
+#             rightHeights = []
+#             findAllLeafsHeight(node.left, 1, leftHeights)
+#             findAllLeafsHeight(node.right, 1, rightHeights)
+
+#             leftHeights.sort()
+#             rightHeights.sort()
+
+#             for leftHeight in leftHeights:
+#                 if leftHeight >= distance: break
+
+#                 for rightHeight in rightHeights:
+#                     if leftHeight + rightHeight <= distance:
+#                         cnt += 1
+#                     else:
+#                         break
+
+#             return cnt + dfs(node.left, distance) + dfs(node.right, distance)
+
+#         return dfs(root, distance)
+
+# 需要使用一个全局变量来保存结果
 class Solution:
+    def __init__(self):
+        self.res = 0
+    
     def countPairs(self, root: TreeNode, distance: int) -> int:
-        def findAllLeafsHeight(node, currHeight, heights):
-            if not node: return []
-
-            if not node.left and not node.right:
-                heights.append(currHeight)
-                return
-
-            findAllLeafsHeight(node.left, currHeight + 1, heights)
-            findAllLeafsHeight(node.right, currHeight + 1, heights)
-
+        # 返回当前结点的所有子节点高度（小于 distance）
         def dfs(node, distance):
-            if not node: return 0
+            if not node: return []  # 节点为 null，返回空数组
+            if not node.left and not node.right: return [1] # 节点为叶子节点，那么对于上一层来说，该叶子节点高度为 1
+            
+            # 左叶子节点高度和右叶子节点高度
+            leftHeights = dfs(node.left, distance)
+            rightHeights = dfs(node.right, distance)
+            
+            # 找到所有 左高度+右高度<=distance 的组合个数
+            self.res += sum([l + r <= distance for l in leftHeights for r in rightHeights])
+                        
+            # 对于上一层，高度要 + 1
+            return [n + 1 for n in leftHeights + rightHeights if n + 1 < distance]
+        
+        dfs(root, distance)
+        return self.res
 
-            cnt = 0
-            leftHeights = []
-            rightHeights = []
-            findAllLeafsHeight(node.left, 1, leftHeights)
-            findAllLeafsHeight(node.right, 1, rightHeights)
-
-            leftHeights.sort()
-            rightHeights.sort()
-
-            for leftHeight in leftHeights:
-                if leftHeight >= distance: break
-
-                for rightHeight in rightHeights:
-                    if leftHeight + rightHeight <= distance:
-                        cnt += 1
-                    else:
-                        break
-
-            return cnt + dfs(node.left, distance) + dfs(node.right, distance)
-
-        return dfs(root, distance)
 
 root = TreeNode(1)
 root.left = TreeNode(2)
