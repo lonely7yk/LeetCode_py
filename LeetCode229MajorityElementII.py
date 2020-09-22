e32df8ef0d46770923ef6e8835ad961828a92e58
lonely7yk <893077810@qq.com>
2020-09-22 16:42:09 -0700

    LeetCode229 More Voting

--- LeetCode229MajorityElementII.py
+++ LeetCode229MajorityElementII.py
@@ -16,31 +16,59 @@ Output: [1,2]
 
 from typing import List
 
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> List[int]:
+#         if not nums or len(nums) <= 1: return nums
+
+#         cnt1, cnt2, candidate1, candidate2 = 0, 0, 0, 1 # 要确保 candidate1 和 candidate2 不等
+
+#         for num in nums:
+#             # 1. 判断 num 是否是候选值，如果是，cnt 加1
+#             # 2. 判断 cnt 是否为 0，如果是，更新候选值和 cnt
+#             # 3. 1 和 2 都不满足，则 cnt 减1
+#             if candidate1 == num:
+#                 cnt1 += 1
+#             elif candidate2 == num:
+#                 cnt2 += 1
+#             elif cnt1 == 0:
+#                 candidate1 = num
+#                 cnt1 = 1
+#             elif cnt2 == 0:
+#                 candidate2 = num
+#                 cnt2 = 1
+#             else:
+#                 cnt1 -= 1
+#                 cnt2 -= 1
+
+#         return [x for x in [candidate1, candidate2] if nums.count(x) > len(nums) // 3]
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if not nums or len(nums) <= 1: return nums
-
-        cnt1, cnt2, candidate1, candidate2 = 0, 0, 0, 1 # 要确保 candidate1 和 candidate2 不等
-
+        cnt1 = 0
+        cnt2 = 0
+        last1 = None
+        last2 = None
+        n = len(nums)
+        
         for num in nums:
-            # 1. 判断 num 是否是候选值，如果是，cnt 加1
-            # 2. 判断 cnt 是否为 0，如果是，更新候选值和 cnt
-            # 3. 1 和 2 都不满足，则 cnt 减1
-            if candidate1 == num:
+            if num == last1:
                 cnt1 += 1
-            elif candidate2 == num:
+            elif num == last2:
                 cnt2 += 1
-            elif cnt1 == 0:
-                candidate1 = num
-                cnt1 = 1
-            elif cnt2 == 0:
-                candidate2 = num
-                cnt2 = 1
             else:
-                cnt1 -= 1
-                cnt2 -= 1
+                if cnt1 == 0:
+                    last1 = num
+                    cnt1 = 1
+                elif cnt2 == 0:
+                    last2 = num
+                    cnt2 = 1
+                else:
+                    cnt1 -= 1
+                    cnt2 -= 1
+                    
+        return [x for x in [last1, last2] if nums.count(x) > n // 3]
 
-        return [x for x in [candidate1, candidate2] if nums.count(x) > len(nums) // 3]
 
 nums = [3,3]
 # nums = [1,1,1,3,3,2,2,2]
