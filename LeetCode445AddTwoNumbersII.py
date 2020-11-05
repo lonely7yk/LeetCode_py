68d5d0e08724e58f89a88e5308b99f8f236a6ca0
lonely7yk <893077810@qq.com>
2020-11-05 15:48:26 -0800

    LeetCode455 LinkedList

--- LeetCode445AddTwoNumbersII.py
+++ LeetCode445AddTwoNumbersII.py
@@ -47,3 +47,135 @@ class Solution:
         return head.next if head.next else ListNode(0)
 
 
+# # reverse list + head insertion: O(n) - O(1)
+# class Solution:
+#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+#         def reverseList(head):
+#             pre, cur = None, head
+            
+#             while cur:
+#                 cur.next, cur, pre = pre, cur.next, cur
+                
+#             return pre
+        
+#         l1 = reverseList(l1)
+#         l2 = reverseList(l2)
+        
+#         dumb = ListNode(-1)
+#         carry = 0
+#         while l1 or l2:
+#             l1Val = l1.val if l1 else 0
+#             l2Val = l2.val if l2 else 0
+#             val = l1Val + l2Val + carry
+            
+#             carry = val // 10
+#             dumb.next = ListNode(val % 10, dumb.next)
+            
+#             l1 = l1.next if l1 else l1
+#             l2 = l2.next if l2 else l2
+            
+#         if carry: dumb.next = ListNode(carry, dumb.next)
+#         return dumb.next
+
+
+# # not reverse list: O(n) - O(1)
+# class Solution:
+#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+#         n1 = 0
+#         n2 = 0
+#         p1, p2 = l1, l2
+#         # 计算 l1 和 l2 的长度
+#         while p1:
+#             n1 += 1
+#             p1 = p1.next
+            
+#         while p2:
+#             n2 += 1
+#             p2 = p2.next
+            
+#         dumb = ListNode(-1)
+#         p1, p2 = l1, l2
+#         # 把所有对应的node加起来（不考虑进位），然后头插法插入到一个新list
+#         while n1 or n2:
+#             val = 0
+#             if n1 >= n2:
+#                 val += p1.val
+#                 n1 -= 1
+#                 p1 = p1.next
+#             if n1 < n2:
+#                 val += p2.val
+#                 n2 -= 1
+#                 p2 = p2.next
+                
+#             dumb.next = ListNode(val, dumb.next)
+            
+#         p = dumb.next
+#         dumb2 = ListNode(-1)
+#         carry = 0
+#         # 遍历新 list，考虑进位更新每一位，并用头插法插入到另一个新 list 中
+#         # while p:
+#         #     val = p.val + carry
+#         #     dumb2.next = ListNode(val % 10, dumb2.next)
+#         #     carry = val // 10
+#         #     p = p.next
+#         while p:
+#             p.val += carry
+#             carry = p.val // 10
+#             p.val %= 10
+            
+#             tmp = p.next
+#             p.next = dumb2.next
+#             dumb2.next = p
+#             p = tmp
+            
+#         # 如果还有多的进位，插在头上
+#         if carry: dumb2.next = ListNode(carry, dumb2.next)
+#         return dumb2.next
+
+
+# recursive: O(n)
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # offset 表示 p1 比 p2 多多少个长度
+        def dfs(p1, p2, offset):
+            if not p1 and not p2: return None
+            
+            cur = None
+            nxt = None
+            if offset > 0:
+                cur = ListNode(p1.val)
+                nxt = dfs(p1.next, p2, offset - 1)
+            else:
+                cur = ListNode(p1.val + p2.val)
+                nxt = dfs(p1.next, p2.next, 0)
+                
+            # nxt 如果大于 10，进位给 cur
+            if nxt and nxt.val >= 10:
+                cur.val += nxt.val // 10
+                nxt.val %= 10
+                
+            cur.next = nxt
+            return cur
+        
+        n1, n2 = 0, 0
+        p1, p2 = l1, l2
+        # 计算 l1 和 l2 长度
+        while p1:
+            p1 = p1.next
+            n1 += 1
+        while p2:
+            p2 = p2.next
+            n2 += 1
+            
+        res = None
+        if n1 < n2:
+            res = dfs(l2, l1, n2 - n1)
+        else:
+            res = dfs(l1, l2, n1 - n2)
+            
+        if res.val >= 10:
+            res = ListNode(res.val // 10, res)
+            res.next.val %= 10
+            
+        return res
+
