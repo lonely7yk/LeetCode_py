6bab2fef2e744dc2fb5fc9f60d7f8013c4feb2dd
lonely7yk <893077810@qq.com>
2020-11-09 16:40:29 -0800

    LeetCode046 DFS

--- LeetCode046Permutations.py
+++ LeetCode046Permutations.py
@@ -17,27 +17,49 @@ Output:
 
 from typing import List
 
+# class Solution:
+#     # DFS: O(n!) 36ms 81%
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         def findPermutationDFS(curr, res, nums):
+#             if not nums:
+#                 res.append(list(curr))
+#                 return
+
+#             for i in range(len(nums)):
+#                 curr.append(nums[i])
+#                 tmp = nums[:i] + nums[i+1:]     # 删除 nums[i]
+#                 findPermutationDFS(curr, res, tmp)
+#                 curr.pop()
+
+#         if not nums: return []
+
+#         curr = []
+#         res = []
+#         findPermutationDFS(curr, res, nums)
+
+#         return res
+
+
+# O(n!) without extra memory
 class Solution:
-    # DFS: O(n!) 36ms 81%
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def findPermutationDFS(curr, res, nums):
-            if not nums:
-                res.append(list(curr))
+        def dfs(first):
+            if first == n:
+                res.append(nums[:])
                 return
-
-            for i in range(len(nums)):
-                curr.append(nums[i])
-                tmp = nums[:i] + nums[i+1:]     # 删除 nums[i]
-                findPermutationDFS(curr, res, tmp)
-                curr.pop()
-
-        if not nums: return []
-
-        curr = []
+            
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                dfs(first + 1)
+                nums[i], nums[first] = nums[first], nums[i]
+                
+            
+        n = len(nums)
         res = []
-        findPermutationDFS(curr, res, nums)
-
+        dfs(0)
         return res
+
+
 
 if __name__ == '__main__':
     nums = [1,2,3]
