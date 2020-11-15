a329cbfd6d28e0f5699d4c6a68f4598d756a33b6
lonely7yk <893077810@qq.com>
2020-11-14 22:54:10 -0800

    LeetCode1657 HashMap

---
+++ LeetCode1000/LeetCode1657DetermineifTwoStringsAreClose.py
@@ -0,0 +1,64 @@
+"""
+Two strings are considered close if you can attain one from the other using the following operations:
+
+Operation 1: Swap any two existing characters.
+For example, abcde -> aecdb
+Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+You can use the operations on either string as many times as necessary.
+
+Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "bca"
+Output: true
+Explanation: You can attain word2 from word1 in 2 operations.
+Apply Operation 1: "abc" -> "acb"
+Apply Operation 1: "acb" -> "bca"
+
+Example 2:
+
+Input: word1 = "a", word2 = "aa"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+
+Example 3:
+
+Input: word1 = "cabbba", word2 = "abbccc"
+Output: true
+Explanation: You can attain word2 from word1 in 3 operations.
+Apply Operation 1: "cabbba" -> "caabbb"
+Apply Operation 2: "caabbb" -> "baaccc"
+Apply Operation 2: "baaccc" -> "abbccc"
+
+Example 4:
+
+Input: word1 = "cabbba", word2 = "aabbss"
+Output: false
+Explanation: It is impossible to attain word2 from word1, or vice versa, in any amount of operations.
+
+Constraints:
+
+1 <= word1.length, word2.length <= 10^5
+word1 and word2 contain only lowercase English letters.
+"""
+
+import collections
+
+
+# 本质判断两个条件：1. 两个 word 包含的单词是否一样   2. 两个 word 中字母频率排序后是否一致
+# O(n) - O(1)
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        cntMap1 = collections.Counter(word1)
+        cntMap2 = collections.Counter(word2)
+
+        charSet1 = cntMap1.keys()
+        charSet2 = cntMap2.keys()
+        if charSet1 != charSet2: return False
+
+        cnt1 = sorted(list(cntMap1.values()))
+        cnt2 = sorted(list(cntMap2.values()))
+
+        return cnt1 == cnt2
