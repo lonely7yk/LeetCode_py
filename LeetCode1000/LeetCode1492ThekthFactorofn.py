785b12b64933eb87133efc58655c25fc6bbb958d
lonely7yk <893077810@qq.com>
2020-12-05 17:00:46 -0800

    LeetCode1492

---
+++ LeetCode1000/LeetCode1492ThekthFactorofn.py
@@ -0,0 +1,77 @@
+"""
+Given two positive integers n and k.
+
+A factor of an integer n is defined as an integer i where n % i == 0.
+
+Consider a list of all factors of n sorted in ascending order, return the kth factor in this list 
+or return -1 if n has less than k factors.
+
+Example 1:
+
+Input: n = 12, k = 3
+Output: 3
+Explanation: Factors list is [1, 2, 3, 4, 6, 12], the 3rd factor is 3.
+
+Example 2:
+
+Input: n = 7, k = 2
+Output: 7
+Explanation: Factors list is [1, 7], the 2nd factor is 7.
+
+Example 3:
+
+Input: n = 4, k = 4
+Output: -1
+Explanation: Factors list is [1, 2, 4], there is only 3 factors. We should return -1.
+
+Example 4:
+
+Input: n = 1, k = 1
+Output: 1
+Explanation: Factors list is [1], the 1st factor is 1.
+
+Example 5:
+
+Input: n = 1000, k = 3
+Output: 4
+Explanation: Factors list is [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000].
+
+Constraints:
+
+1 <= k <= n <= 1000
+"""
+
+
+# # O(n)
+# class Solution:
+#     def kthFactor(self, n: int, k: int) -> int:
+#         if k == 1: return 1
+#         cnt = 1
+        
+#         for i in range(2, n + 1):
+#             if n % i == 0:
+#                 cnt += 1
+#                 if cnt == k:
+#                     return i
+                
+#         return -1
+
+
+# O(n^0.5)
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        sqrt_n = int(n ** 0.5)
+        factors = []
+        
+        # 只记录 1 到 根号n 范围内的 factor
+        for i in range(1, sqrt_n + 1):
+            if n % i == 0:
+                k -= 1
+                if k == 0: return i
+                factors.append(i)
+            
+        # 如果 n 是 sqrt_n 的平方，k 自增 1
+        if sqrt_n * sqrt_n == n: k += 1
+        lenFactor = len(factors)
+        return n // factors[lenFactor - k] if lenFactor >= k else -1
+
